09fbae4aafd19d55aac92d56d6389e34846437ea
James Corcoran <jpcsoccer1234@gmail.com>
2021-06-24 15:20:59 -0400

    finished update route

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -5,20 +5,43 @@ module.exports = function(app){
         try {
             const workouts = await Workout.find({}).sort({ day: 1 }).exec();
             res.json(workouts);
-          } catch (error) {
-            res.json(error);
-          }
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err);
+        };
     });
 
     app.get('/api/workouts/range', async (req, res) => {
-        const workouts = await db.Workout.aggregate([{
+        try {
+            const workouts = await db.Workout.aggregate([{
             $addFields: {
                 totalDuration: { $sum : "$exercises.duration"}
             } 
-        }])
-        const lastSeven = allWorkoutsData.slice(-7);
-        res.json(lastSeven);
-    })
+        }]);
+            const lastSeven = allWorkoutsData.slice(-7);
+            res.json(lastSeven);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err);
+        };
+    });
 
-    
+    app.put('/api/workouts/:id', async (req, res) => {
+        try {
+            const workoutUpdated = await Workout.updateOne(
+                { _id: req.params.id },
+                { $push: { exercises: req.body }}
+            );
+            res.json(workoutUpdated);
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err);
+        };
+    });
+
+    app.post("/api/workouts", async ({ body }, res) => {
+        try {
+            
+        } catch {};
+    });
 }
