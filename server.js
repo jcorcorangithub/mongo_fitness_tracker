dcec97e3c20b0696415470130fcfce26bddb3dae
James Corcoran <jpcsoccer1234@gmail.com>
2021-06-24 15:35:36 -0400

    corrected route to html routes and began to correct paths to html and styling for pages

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("public/html"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./controllers/apiRoutes")(app);
+require("./controllers/htmlRoutes");
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
